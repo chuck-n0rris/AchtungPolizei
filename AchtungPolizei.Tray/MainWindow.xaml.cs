1fd9fc6c3539e146f4535d2d5fd23a2d16bfb97e
chuck-n0rris <kurilenkodenis@gmail.com>
2012-02-16 23:36:30 +0200

    Added tray client for WPF

---
+++ AchtungPolizei.Tray/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+
+namespace AchtungPolizei.Tray
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Called when settings button was cliecked.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">
+        /// The <see cref="System.Windows.RoutedEventArgs"/> 
+        /// instance containing the event data.
+        /// </param>
+        private void OnSettingsClick(object sender, RoutedEventArgs e)
+        {
+            var settings = new Settings();
+            settings.ShowDialog();
+        }
+
+        /// <summary>
+        /// Called when exit button was cliecked.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">
+        /// The <see cref="System.Windows.RoutedEventArgs"/> instance
+        /// containing the event data.
+        /// </param>
+        private void OnExitClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ AchtungPolizei.Tray/Settings.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace AchtungPolizei.Tray
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class Settings
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Settings"/> class.
+        /// </summary>
+        public Settings()
+        {
+            InitializeComponent();
+        }
+    }
+}
