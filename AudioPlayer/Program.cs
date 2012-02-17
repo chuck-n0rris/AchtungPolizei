b10556f35c1c90373cfd13ef7582c456a3249230
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-02-17 18:02:28 +0200

    Added branch with audio player POC.

---
+++ AudioPlayer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+
+namespace AudioPlayer
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var device = new WasapiOut(AudioClientShareMode.Shared, 100))
+            using (var stream = new WaveChannel32(new Mp3FileReader("aircraft012.mp3")))
+            {
+                device.Init(stream);
+                DoOnKeyStroke(() => device.Play(), "play");
+                DoOnKeyStroke(() => device.Stop(), "stop");
+                DoOnKeyStroke(() => {}, "exit");
+            }
+        }
+
+        private static void DoOnKeyStroke(Action action, string actionName)
+        {
+            Console.WriteLine(string.Format("Press any key to {0}...", actionName));
+            Console.ReadKey();
+            action();
+        }
+    }
+}
---
+++ AudioPlayer/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Audio Player POC")]
+[assembly: AssemblyProduct("Audio Player POC")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
