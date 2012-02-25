5ed2c49fb5a6c03c659c960bff08dd86aa27ad65
Sergey Barinov <sergey.barinov@binary-studio.com>
2012-02-25 16:50:20 +0200

    Implement simple TextToSpeech output plugin

---
+++ AchtungPolizei.Plugins.TextToSpeech/TextToSpeechPlugin.cs
@@ -0,0 +1,67 @@
+﻿namespace AchtungPolizei.Plugins.TextToSpeech
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Speech.Synthesis;
+    using System.Threading.Tasks;
+    
+    using AchtungPolizei.Plugins;
+
+    public class TextToSpeechPlugin : IOutputPlugin
+    {
+        private readonly Dictionary<BuildStatus, string> statusToText = new Dictionary<BuildStatus, string>
+            {
+                { BuildStatus.Broken, "Build has been broken."},
+                { BuildStatus.StillBroken, "Build is still broken."},
+                { BuildStatus.Fixed, "Build has been fixed."}
+            };
+
+        private readonly SpeechSynthesizer synthesizer;
+
+        private readonly Guid id = new Guid("603e7da9-4cb1-4ac7-b84e-7ce12b3cbee3");
+
+        private const string name = "Text to Speech";
+
+        private bool disposed;
+
+        public TextToSpeechPlugin()
+        {
+            synthesizer = new SpeechSynthesizer();
+            synthesizer.SetOutputToDefaultAudioDevice();
+        }
+
+        public void Dispose()
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            synthesizer.Dispose();
+
+            disposed = true;
+        }
+
+        public Guid Id
+        {
+            get { return id; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public Task Start(BuildState state, BuildStatus status)
+        {
+            return new Task(Speak, statusToText[status]);
+        }
+
+        private void Speak(object text)
+        {
+            var builder = new PromptBuilder();
+            builder.AppendText((string)text);
+            synthesizer.Speak(builder);
+        }
+    }
+}
---
+++ AchtungPolizei.Plugins.TextToSpeechTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using AchtungPolizei.Plugins.TextToSpeech;
+
+namespace AchtungPolizei.Plugins.TextToSpeechTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            var plugin = new TextToSpeechPlugin();
+            var buildState = new BuildState();
+
+            Task sayBuildFixed = plugin.Start(buildState, BuildStatus.Fixed);
+            sayBuildFixed.RunSynchronously();
+
+            Task sayBuildBroken = plugin.Start(buildState, BuildStatus.Broken);
+            sayBuildBroken.RunSynchronously();
+
+            Task sayBuildStillBroken = plugin.Start(buildState, BuildStatus.StillBroken);
+            sayBuildStillBroken.RunSynchronously();
+        }
+    }
+}
