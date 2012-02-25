397d9520905d215e4b469fe8390d87579b105819
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-02-26 01:01:46 +0200

    Implemented project repository.

--- AchtungPolizei.Core/ProjectsRepository.cs
+++ AchtungPolizei.Core/ProjectsRepository.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
 
 namespace AchtungPolizei.Core
 {
@@ -7,12 +10,41 @@ namespace AchtungPolizei.Core
     {
         public IEnumerable<Project> GetProjects()
         {
-            throw new NotImplementedException();
+            var settings = GetSettingsPath();
+            try
+            {
+                return JsonConvert.DeserializeObject<List<Project>>(
+                    File.ReadAllText(settings));
+            }
+            catch (Exception)
+            {
+                return new List<Project>();
+            }
         }
 
         public void SaveProjects(IEnumerable<Project> projects)
         {
-            throw new NotImplementedException();
+            File.WriteAllText(
+                GetSettingsPath(),
+                JsonConvert.SerializeObject(
+                    projects.ToList(),
+                    Formatting.Indented));
+        }
+
+        private string GetSettingsPath()
+        {
+            var applicationData = Environment.GetFolderPath(
+                Environment.SpecialFolder.ApplicationData);
+
+            applicationData = Path.Combine(
+                applicationData, "AchtungPolizei");
+
+            if (!Directory.Exists(applicationData))
+            {
+                Directory.CreateDirectory(applicationData);
+            }
+
+            return Path.Combine(applicationData, "settings.config");
         }
     }
 }
