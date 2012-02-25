5fcbae8c06daa0de52494e2de25c68ebd10fe7f6
dv00d00 <dmykush@gmail.com>
2012-02-25 20:15:22 +0200

    Moved plugions to common folder

--- AchtungPolizei.Plugins.Impl/SoundOutputPlugin.cs
+++ AchtungPolizei.Plugins.Impl/SoundOutputPlugin.cs
@@ -1,39 +1,8 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
-using NAudio.CoreAudioApi;
-using NAudio.Wave;
 
-namespace AchtungPolizei.Plugins
+namespace AchtungPolizei.Plugins.Impl
 {
-    /// ѕришлось писать наследника дл€ того чтобы пон€ть когда дошли до конца файла
-    public class TrackableWaveChannel : WaveChannel32
-    {
-        public TrackableWaveChannel(WaveStream sourceStream, float volume, float pan)
-            : base(sourceStream, volume, pan)
-        {
-        }
-
-        public TrackableWaveChannel(WaveStream sourceStream)
-            : base(sourceStream)
-        {
-        }
-
-        public override int Read(byte[] destBuffer, int offset, int numBytes)
-        {
-            var result = base.Read(destBuffer, offset, numBytes);
-
-            if (Position >= Length && Finished != null)
-            {
-                Finished(this, new EventArgs());
-            }
-
-            return result;
-        }
-
-        public event EventHandler Finished;
-    }
-
     public class SoundOutputPlugin : IOutputPlugin
     {
         private readonly Guid guid = Guid.Parse("282356A9-3E91-4405-B54B-072709E1DA09");
