f08b82c027bcec939fae68a7c36801957c057d38
Sergey Barinov <sergey.barinov@binary-studio.com>
2012-02-26 00:03:12 +0200

    Plugin locator gives all instances or by id.

--- AchtungPolizei.Core/Engine.cs
+++ AchtungPolizei.Core/Engine.cs
@@ -1,4 +1,6 @@
-﻿namespace AchtungPolizei.Core
+﻿using AchtungPolizei.Plugins;
+
+namespace AchtungPolizei.Core
 {
     using System;
     using System.Collections.Generic;
@@ -7,6 +9,10 @@
     {
         private List<Project> projects = new List<Project>();
 
+        private List<Type> pluginTypes = new List<Type>();
+
+        private List<IInputPlugin> projectsInputPlugins = new List<IInputPlugin>();
+
         public event EventHandler<ProjectStatusChangedArgs> ProjectStatusChanged;
 
         public void Start(List<Project> projects)
@@ -14,7 +20,9 @@
             this.projects = projects;
         }
 
-        private void ActivateInputPlugin()
+        
+
+        private void ActivateInputPlugin(Project project)
         {
             
         }
