c956835a7c9ff3717011ba10a64a22f80db46fde
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-02-26 03:16:46 +0200

    Fixed merge error.

--- AchtungPolizei.Tray/ProjectViewModel.cs
+++ AchtungPolizei.Tray/ProjectViewModel.cs
@@ -1,36 +1,28 @@
-using AchtungPolizei.Core;
-
 namespace AchtungPolizei.Tray
 {
-    /// <summary>
-    /// The project view model.
-    /// </summary>
-    public class ProjectViewModel : ViewModelBase
+    using AchtungPolizei.Core;
+    using AchtungPolizei.Plugins;
+
+    public class ProjectViewModel : ViewModelBase<ProjectViewModel>
     {
         private string name;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ProjectViewModel"/> class.
-        /// </summary>
-        /// <param name="project">
-        /// The project.
-        /// </param>
         public ProjectViewModel(Project project)
         {
-            Name = project.Name;
+            this.Name = project.Name;
         }
 
-        /// <summary>
-        /// Gets or sets Name.
-        /// </summary>
         public string Name
         {
-            get { return name; }
+            get
+            {
+                return this.name;
+            }
 
             set
             {
-                name = value;
-                RaisePropertyChanged("Name");
+                this.name = value;
+                this.RaisePropertyChanged("Name");
             }
         }
     }
