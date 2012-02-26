cd4eb05637b7655a98fc3eb121c8fa4e84f00584
chuck-n0rris <kurilenkodenis@gmail.com>
2012-02-26 04:09:28 +0200

    Added Update project control and changes in ProjectViewModel

---
+++ AchtungPolizei.Tray/UpdateProject.xaml.cs
@@ -0,0 +1,93 @@
+﻿namespace AchtungPolizei.Tray
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    using AchtungPolizei.Core;
+
+    /// <summary>
+    /// Interaction logic for CreateUpdateProject.xaml
+    /// </summary>
+    public partial class UpdateProject
+    {
+        private readonly UpdateProjectViewModel model;
+
+        public UpdateProject(Project project)
+        {
+            InitializeComponent();
+
+            model = new UpdateProjectViewModel(project);
+            DataContext = model;
+        }
+
+        public Project CreatedProject
+        {
+            get
+            {
+                return model.GetProject();
+            }
+        }
+
+        private void AddOutputPluginClick(object sender, RoutedEventArgs e)
+        {
+            if (model.SelectedOutputPlugin != null)
+            {
+                if (model.SelectedOutputPlugin.Configuration.Validate())
+                {
+                    model.SelectedOutputPlugins.Add(new PluginViewModel
+                    {
+                        Id = model.SelectedOutputPlugin.Id,
+                        Name = model.SelectedOutputPlugin.Name,
+                        Configuration = model.SelectedOutputPlugin.Configuration
+                    });
+                    
+                    model.SelectedOutputPlugin = null;
+                    OutputPluginsComboBox.SelectedItem = null;
+                }
+            }
+        }
+
+        private void CancelButtonClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void CreateButtonClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void InputPluginSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (InputPluginsComboBox.SelectedItem == null)
+                return;
+
+            var pluginPreview = (PluginPreviewItem)InputPluginsComboBox.SelectedItem;
+            var pluginModel = new PluginViewModel
+            {
+                Id = pluginPreview.Id,
+                Name = pluginPreview.Name,
+                Configuration = pluginPreview.Source.GetConfigControl()
+            };
+            
+            model.SelectedInputPlugin = pluginModel;
+        }
+
+        private void OutputPluginSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (OutputPluginsComboBox.SelectedItem == null)
+                return;
+            
+            var pluginPreview = (PluginPreviewItem)OutputPluginsComboBox.SelectedItem;
+
+            var pluginModel = new PluginViewModel
+            {
+                Id = pluginPreview.Id,
+                Name = pluginPreview.Name,
+                Configuration = pluginPreview.Source.GetConfigControl()
+            };
+
+            model.SelectedOutputPlugin = pluginModel;
+        }
+    }
+}
---
+++ AchtungPolizei.Tray/UpdateProjectViewModel.cs
@@ -0,0 +1,167 @@
+namespace AchtungPolizei.Tray
+{
+    using System.Collections.ObjectModel;
+    using System.Linq;
+
+    using AchtungPolizei.Core;
+    using AchtungPolizei.Core.Helpers;
+    using AchtungPolizei.Plugins;
+
+    public class UpdateProjectViewModel : ViewModelBase<CreateProjectViewModel>
+    {
+        private Project internalProject;
+
+        private string name;
+
+        private ObservableCollection<PluginPreviewItem> inputPlugins;
+        private ObservableCollection<PluginPreviewItem> outputPlugins;
+
+        private PluginViewModel selectedInputPlugin;
+        private PluginViewModel selectedOutputPlugin;
+
+        private ObservableCollection<PluginViewModel> selectedOutputPlugins;
+        
+        public UpdateProjectViewModel(Project project)
+        {
+            this.internalProject = project;
+
+            this.InputPlugins = new ObservableCollection<PluginPreviewItem>();
+            this.OutputPlugins = new ObservableCollection<PluginPreviewItem>();
+            this.SelectedOutputPlugins = new ObservableCollection<PluginViewModel>();
+
+            this.Name = project.Name;
+            
+            this.InitializeCollections();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                this.name = value;
+                this.RaisePropertyChanged("Name");
+            }
+        }
+
+        public PluginViewModel SelectedInputPlugin
+        {
+            get
+            {
+                return this.selectedInputPlugin;
+            }
+
+            set
+            {
+                this.selectedInputPlugin = value;
+                this.RaisePropertyChanged("SelectedInputPlugin");
+            }
+        }
+
+        public PluginViewModel SelectedOutputPlugin
+        {
+            get
+            {
+                return this.selectedOutputPlugin;
+            }
+
+            set
+            {
+                this.selectedOutputPlugin = value;
+                this.RaisePropertyChanged("SelectedOutputPlugin");
+            }
+        }
+
+        public ObservableCollection<PluginViewModel> SelectedOutputPlugins
+        {
+            get
+            {
+                return this.selectedOutputPlugins;
+            }
+            set
+            {
+                this.selectedOutputPlugins = value;
+                this.RaisePropertyChanged("OutputConfigurationControls");
+            }
+        }
+
+        public ObservableCollection<PluginPreviewItem> InputPlugins
+        {
+            get
+            {
+                return this.inputPlugins;
+            }
+
+            set
+            {
+                this.inputPlugins = value;
+                this.RaisePropertyChanged("InputPlugins");
+            }
+        }
+
+        public ObservableCollection<PluginPreviewItem> OutputPlugins
+        {
+            get
+            {
+                return this.outputPlugins;
+            }
+
+            set
+            {
+                this.outputPlugins = value;
+                this.RaisePropertyChanged("OutputPlugins");
+            }
+        }
+        
+        public Project GetProject()
+        {
+            var project = new Project();
+            project.Name = this.Name;
+
+            project.InputPlugin = new PluginConfiguration
+            {
+                PluginId = SelectedInputPlugin.Id,
+                Configuration = SelectedInputPlugin.Configuration.GetConfiguration()
+            };
+
+            foreach (var pluginModel in SelectedOutputPlugins)
+            {
+                project.OutputPlugins.Add(
+                    new PluginConfiguration
+                    {
+                        PluginId = pluginModel.Id,
+                        Configuration = pluginModel.Configuration.GetConfiguration()
+                    });
+            }
+
+            return project;
+        }
+
+        private void InitializeCollections()
+        {
+            foreach (var inputPlugin in PluginLocator.Current.GetAllInputPlugins())
+            {
+                this.InputPlugins.Add(new PluginPreviewItem(inputPlugin));
+            }
+
+            foreach (var outputPlugin in PluginLocator.Current.GetAllOutputPlugins())
+            {
+                this.OutputPlugins.Add(new PluginPreviewItem(outputPlugin));
+            }
+
+            var foundPlugin = InputPlugins.Single(it => it.Id == this.internalProject.InputPlugin.PluginId);
+            var pluginModel = new PluginViewModel
+                {
+                    Id = foundPlugin.Id,
+                    Name = foundPlugin.Name
+                    // Configuration = internalProject.InputPlugin.Configuration
+                };
+
+            // SelectedInputPlugin = 
+        }
+    }
+}
